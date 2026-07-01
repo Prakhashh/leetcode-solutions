6a1378b9432d705fed21188f028f324971384bc3
Prem <premprakash8539@gmail.com>
2026-07-01 11:41:23 +0530

    Time: 4 ms (31.66%), Space: 8.7 MB (38.15%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int rev = 0;
+        int dup = x;
+        while( x > 0 ){
+            int digit = x % 10;
+
+        if (rev > INT_MAX / 10 || rev < INT_MIN / 10)
+    return false;    // or return 0 for Reverse Integer problem
+
+
+  
+            rev = rev * 10 + digit;
+            x = x / 10;
+        }
+        if (rev == dup){
+            return true;
+        } else{
+            return false;
+        }
+    }
+};
